c51261af9a46d07cf10fbd6b5100a65e60a0fda9
Rapheal Ajiboye <raphoooo95@gmail.com>
2023-03-16 22:41:42 +0100

    fixed header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -11,7 +11,7 @@ const Header = () => {
     <div className='app__header app__wrapper section__padding' id="home">
       <div className='app_wrapper_info'>
         <SubHeading title="...Kukuruku, cook that thing..."/>
-        <h1 className='app__header-h1' style={{marginTop: "1rem"}}>Your favourite AltSchool Chef</h1>
+        <h1 className='app__header-h1' style={{marginTop: "1rem"}}>Your favourite Chef</h1>
         <p className='p__opensans' style={{margin:"2rem 0"}}>Are you looking for the right place to get quality meal at affordable prices? Chat with our bot now...</p>
         {/* <button type='button' className='custom__button'>Explore menu</button> */}
       </div>
